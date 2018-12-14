70693aa19cc417ea477ef510cc284b582df237f6
Your Name <you@example.com>
2018-12-14 15:17:30 +0100

    add Controller

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
+
+public class Controller {
+
+    @GetMapping(value = "/helloex", produces = "application/json")
+    public ResponseEntity<String> testExceptionHandling() {
+      return ResponseEntity.ok("Salamou Alaykom V1") ;
+    }
+
+}
